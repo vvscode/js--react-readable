1a39add5c3c407b860bb149b4e3d65a28b5e2caf
Vasiliy Vanchuk <v.vanchuk@tut.by>
2017-09-18 19:03:54 +0300

    Create base routes

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+
+import "./App.css";
+import HomeScreen from "./screens/HomeScreen";
+import CategoryScreen from "./screens/CategoryScreen";
+import PostScreen from "./screens/PostScreen";
+import PostEditScreen from "./screens/PostEditScreen";
+import PostAddScreen from "./screens/PostAddScreen";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <BrowserRouter>
+          <Switch>
+            <Route exact path="/" component={HomeScreen} />
+            <Route
+              exact
+              path="/category/:category"
+              component={CategoryScreen}
+            />
+            <Route exact path="/post/new" component={PostAddScreen} />
+            <Route exact path="/post/:postId" component={PostScreen} />
+            <Route exact path="/post/:postId/edit" component={PostEditScreen} />
+          </Switch>
+        </BrowserRouter>
       </div>
     );
   }
---
+++ src/screens/CategoryScreen.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+const CategoryScreen = () => <div>Category</div>;
+
+export default CategoryScreen;
