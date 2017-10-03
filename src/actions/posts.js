b9cdc2b7204776345c7f0fbb1838d1b84f74e4fc
Vasiliy Vanchuk <v.vanchuk@tut.by>
2017-10-03 20:18:33 +0300

    Add util to fetch posts

--- src/actions/posts.js
+++ src/actions/posts.js
@@ -3,8 +3,10 @@ import * as api from "../utils/api";
 export const POSTS_FETCH = "POSTS_FETCH";
 
 export const fetchPosts = (category = null) => dispatch =>
-  api.fetchPosts(category).then(({ posts }) => ({
-    type: POSTS_FETCH,
-    category,
-    posts
-  }));
+  api.fetchPosts(category).then(posts =>
+    dispatch({
+      type: POSTS_FETCH,
+      category,
+      posts
+    })
+  );
--- src/utils/api.js
+++ src/utils/api.js
@@ -1,3 +1,5 @@
+import uuid from "uuid/v1";
+
 const BASE_URL = "http://localhost:3001";
 const headers = { Authorization: "whatever-you-want" };
 
@@ -10,3 +12,27 @@ export const fetchPosts = (category = null) => {
   const url = category ? `${category}/posts` : "/posts";
   return fetch(getUrl(url), { headers }).then(resp => resp.json());
 };
+
+export const createPost = ({
+  id = uuid(),
+  timestamp = Date.now(),
+  title,
+  body,
+  author = "John Doe",
+  category
+}) =>
+  fetch(getUrl("/posts"), {
+    method: "POST",
+    headers,
+    body: JSON.stringify({
+      id,
+      timestamp,
+      title,
+      body,
+      author,
+      category
+    })
+  }).then(resp => resp.json());
+
+window.fetchPosts = fetchPosts;
+window.createPost = createPost;
