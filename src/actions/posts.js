b0cc2e9a2fefab33f6001f4934a843c36f9d4e4a
Vasiliy Vanchuk <v.vanchuk@tut.by>
2017-10-11 12:19:47 +0300

    Load comment with post

--- src/actions/posts.js
+++ src/actions/posts.js
@@ -1,26 +1,35 @@
 import * as api from "../utils/api";
+import { fetchCommentsByPostid } from "./comments";
 
 export const POSTS_FETCH = "POSTS_FETCH";
 export const POSTS_FETCH_POST = "POSTS_FETCH_POST";
 export const POSTS_ADD = "POSTS_ADD";
 export const POST_UPDATE = "POST_UPDATE";
 
-export const fetchPosts = (category = null) => dispatch =>
-  api.fetchPosts(category).then(posts =>
+export const fetchPosts = (category = null) => dispatch => {
+  const postsPromise = api.fetchPosts(category);
+  postsPromise.then(posts =>
+    posts.forEach(({ id }) => fetchCommentsByPostid(id)(dispatch))
+  );
+
+  return postsPromise.then(posts =>
     dispatch({
       type: POSTS_FETCH,
       category,
       posts
     })
   );
+};
 
-export const fetchPost = id => dispatch =>
+export const fetchPost = id => dispatch => {
+  fetchCommentsByPostid(id)(dispatch);
   api.fetchPost(id).then(post =>
     dispatch({
       type: POSTS_FETCH_POST,
       post
     })
   );
+};
 
 export const addPost = ({ author, title, category, body }) => dispatch =>
   api
